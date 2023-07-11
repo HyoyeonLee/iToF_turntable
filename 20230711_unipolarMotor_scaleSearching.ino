9cac8a143fd63fa2b3ee9e4c2cd2d02fe133df59
HyoyeonLee <60028945+HyoyeonLee@users.noreply.github.com>
2023-07-11 17:13:01 +0900

    Create 20230711_unipolarMotor_scaleSearching.ino
    In order to find the number of steps per a whole revolution, 
    Need some setup with a needle and the protractor like.

---
+++ 20230711_unipolarMotor_scaleSearching.ino
@@ -0,0 +1,61 @@
+//Arduino Code - Move from A to B by a specific number of steps with set speed and acceleration
+
+// Include the Arduino Stepper.h library:
+#include <AccelStepper.h> //Include the AccelStepper library
+
+// Define the motor pins:
+#define MP1  8 // IN1 on the ULN2003
+#define MP2  9 // IN2 on the ULN2003
+#define MP3  10 // IN3 on the ULN2003
+#define MP4  11 // IN4 on the ULN2003
+
+#define MotorInterfaceType 8 // Define the interface type as 8 = 4 wires * step factor (2 for half step)
+#define deg0 0
+#define deg90 900
+#define deg180 1940
+#define deg270 2940
+#define deg360 3980
+AccelStepper stepper = AccelStepper(MotorInterfaceType, MP1, MP3, MP2, MP4);//Define the pin sequence (IN1-IN3-IN2-IN4)
+const int SPR = 3980;//Steps per revolution
+
+int unit = int(round(SPR/4));
+//int unit=5;
+
+
+int inv(int steps)
+{
+  return -steps;
+}
+void setup() {
+  Serial.begin(9600);
+  stepper.setMaxSpeed(500);//Set the maximum motor speed in steps per second
+  stepper.setAcceleration(50);//Set the maximum acceleration in steps per second^2
+  pinMode(2,OUTPUT);
+  pinMode(4,INPUT);
+  pinMode(3,INPUT);
+  stepper.setCurrentPosition(0.0);
+}
+
+void loop() {
+  int read = digitalRead(4);
+  int read2 = digitalRead(3);
+  //Serial.println(read);
+  while(read==HIGH)
+  {
+    stepper.move(inv(unit)); //Set the target motor position (i.e. turn motor for 3 full revolutions)
+    stepper.runToPosition(); // Run the motor to the target position 
+    read = digitalRead(4);
+    Serial.println(inv(stepper.currentPosition()));
+  }
+  while(read2 ==LOW)
+  {
+    stepper.move(inv(-unit)); //Set the target motor position (i.e. turn motor for 3 full revolutions)
+    stepper.runToPosition(); // Run the motor to the target position 
+    read2 = digitalRead(3);
+    Serial.println(inv(stepper.currentPosition()));
+  }
+  Serial.println(inv(stepper.currentPosition()));
+  delay(100);
+
+ // digitalWrite(2,LOW);
+}
